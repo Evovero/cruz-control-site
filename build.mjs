a560014adf9c2b117dd04b2bb1e1651d467185e9
Evovero <admin@evovero.com>
2026-09-11 12:31:04 -0500

    Google Tag

--- build.mjs
+++ build.mjs
@@ -88,7 +88,15 @@ function footer(){
 function layout({title,desc,slug,body,schema='',preloadImg=''}){
   const url = `${biz.domain}/${slug?slug+'/':''}`;
   return `<!DOCTYPE html><html lang="en"><head>
-<meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1">
+<meta charset="utf-8">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XVWCFCR24Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XVWCFCR24Q');
+</script><meta name="viewport" content="width=device-width,initial-scale=1">
 <title>${esc(title)}</title>
 <meta name="description" content="${esc(desc)}">
 <link rel="canonical" href="${url}">
